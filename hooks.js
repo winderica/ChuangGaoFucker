473485dd298abcaf2e879e9fa33cb0bb479ec285
hanyuu <featherin@protonmail.com>
2019-03-03 12:00:18 +0800

    Update: update coordinate

--- hooks.js
+++ hooks.js
@@ -311,6 +311,17 @@ function padZero(value) {
     return "" + ~~value;
 }
 
+function generateCoordinate(x, y) {
+    return {
+        "a": x,
+        "o": y,
+        "v": 1,
+        "s": 0,
+        "t": 0,
+        "ac": 0
+    };
+}
+
 function generateData() {
     var odometer = (3 + Math.random() / 3).toFixed(2);
     var minutes = 15 + Math.random() * 10;
@@ -352,21 +363,22 @@ function generateData() {
     var avgPace = padZero(minutesPerKM) + "'" + padZero(Math.random() * 60) + "''";
     var lastOdometerTime = "00:" + padZero(minutesPerKM + Math.random() * 1.5) + ":" + padZero(Math.random() * 60);
     var stepMinute = (stepCount / minutes).toFixed(2);
-    var beganPoint = "30.508692|114.408546";
-    var endPoint = "30.514006|114.432624";
-    var points = ["30.508692, 114.408546", "30.509981, 114.408303", "30.511087, 114.408132", "30.511424, 114.410362", "30.510868, 114.416998", "30.510521, 114.420856", "30.513120, 114.421127", "30.515302, 114.421338", "30.514605, 114.427742", "30.514145, 114.430993", "30.514006, 114.432624"];
+    var beganX = (30.508 + Math.random() / 10e2).toFixed(6);
+    var beganY = (114.408 + Math.random() / 10e2).toFixed(6);
+    var beganPoint = beganX + "|" + beganY;
+    var endX = (30.514 + Math.random() / 10e2).toFixed(6);
+    var endY = (114.432 + Math.random() / 10e2).toFixed(6);
+    var endPoint = endX + "|" + endY;
+    var points = ["30.509, 114.408", "30.511, 114.408", "30.511, 114.410", "30.510, 114.416", "30.510, 114.420", "30.513, 114.421", "30.515, 114.421", "30.514, 114.427", "30.514, 114.430"];
     var coordinate = [];
+    coordinate.push(generateCoordinate(beganX, beganY));
     for (var i = 0; i < points.length; i++) {
         var point = points[i].split(', ');
-        coordinate.push({
-            "a": +point[0] + ((i === 0 || i === points.length - 1) ? 0 : Math.random() / 10e6),
-            "o": +point[1] + ((i === 0 || i === points.length - 1) ? 0 : Math.random() / 10e6),
-            "v": 1,
-            "s": 0,
-            "t": 0,
-            "ac": 0
-        })
+        var x = +point[0] + ((i === 0 || i === points.length - 1) ? 0 : Math.random() / 10e2);
+        var y = +point[1] + ((i === 0 || i === points.length - 1) ? 0 : Math.random() / 10e2);
+        coordinate.push(generateCoordinate(x, y));
     }
+    coordinate.push(generateCoordinate(endX, endY));
     return {
         calorie: calorie,
         odometer: odometer,
